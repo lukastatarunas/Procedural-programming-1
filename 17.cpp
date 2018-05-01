25ecff27771eff1bf469f922b0a1a40b319dfa97
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-01 22:40:59 +0300

    Create 17.cpp

---
+++ 17.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	int n, c = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			c++;
+		}
+	}
+	if (c == 2)
+	{
+		cout << "TAIP";
+	}
+	else
+	{
+		cout << "NE";
+
+	}
+}
