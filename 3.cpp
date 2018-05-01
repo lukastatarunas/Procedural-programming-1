6a5102692aa72ff9b2da850832aa166aeffce74d
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-01 21:58:13 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{	
+	double a, b, c;
+	cin >> a >> b;
+	c = b * 100 / a - 100;
+	cout << fixed << setprecision(2) << c;
+}
