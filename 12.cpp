267dab11abb055a1d81b2e630893f1427ef6e740
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-01 22:10:25 +0300

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	int max = 0;
+	cin >> a >> b >> c;
+	if (a >=b )
+	{
+		if (a >= c)
+		{
+			max = a;
+		}
+	}
+	if (c >=a)
+	{
+		if (c >= b)
+		{
+			max = c;
+		}
+	}
+	if (b >= a)
+	{
+		if (b >= c)
+		{
+			max = b;
+		}
+	}
+	cout << max;
+}
