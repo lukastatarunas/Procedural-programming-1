413cefe655c3a57f14a26a9fbc6e133977c6475c
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-01 22:01:21 +0300

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{	
+	double a, b;
+	cin >> a >> b;
+	double d;
+	d = 2 * sin(a) + (cos(b) - sqrt(tan(b))) * pow(log10(a), 5);
+	cout << fixed << setprecision(2) << d;
+}
