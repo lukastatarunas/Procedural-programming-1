b42f914cb83c606ca297208b83a6e1da39574696
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-04 15:13:55 +0300

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{	
+	int x1, y1, x2, y2;
+	double a;
+	cin >> x1 >> y1 >> x2 >> y2;
+	a = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	cout << fixed << setprecision(2) << a;
+}
