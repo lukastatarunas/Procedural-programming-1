d8d6d55c92ddaeb0e1bb81620d97041fd20df8c4
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-04 15:15:04 +0300

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{	
+	double p, h, r, V, pi = 3.14159265359;
+	cin >> p >> h >> r;
+	if (p == 1)
+	{
+		V = pi * r * r * h;
+	}
+	if (p == 2)
+	{
+		V = pi * r * r * h / 3;
+	}
+	cout << fixed << setprecision(2) << V;
+}
