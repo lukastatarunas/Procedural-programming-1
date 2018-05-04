e3f93238ccecae83b3b054d2acaec59e81bea3eb
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-04 15:12:37 +0300

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{	
+	int a, b, c;
+	cin >> a >> b >> c;
+	double d;
+	d = (b + sqrt(b*b - a*c)) / (2 * a);
+	cout << fixed << setprecision(3) << d;
+}
