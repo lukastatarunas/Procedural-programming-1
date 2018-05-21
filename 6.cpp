d54db419c2c5c3a0fa35db4fa5a13d2deda0daae
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-21 21:54:33 +0300

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -9,5 +9,5 @@ int main()
 	cin >> x;
 	double d;
 	d = (5 * pow(x, 3) + pow(sin(exp(3 * x) - 16 * pow(x, 2)), 2)) / (sin(4 * x) / (cos(4 * x) + 1) + 1);
-	cout << fixed << setprecision(2) << d;
+	cout << fixed << setprecision(2) << d;a
 }
